d190a28bb64cffdb612b3c8667a4b096919c8009
Juan Esteban Torres <je.torres20@uniandes.edu.co>
2020-12-13 22:24:15 -0500

    first commit

---
+++ busquedas/searchms/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import pymongo
+import datetime
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.parsers import JSONParser
+from django.conf import settings
+from bson.objectid import ObjectId
+
+
+# Create your views here.
+@api_view(["GET"])
+def contenidos(request):
+    client = MongoClient(settings.MONGO_CLI)
+    db = client.booklick
+    contenidos = db['contenidos']
+    result = []
+    buscado=input("Ingrese el titulo o el autor del documento")
+    data = contenidos.find({ $text: { $search: buscado } } )
+    for dto in data:
+        jsonData = {
+            'id': str(dto['_id']),
+            "nombre": dto['variable'],
+            'autor': dto['threshold'],
+            'anio': dto['anio'],
+            'edicion':dto['edicion'],
+            'cuerpo':dto['cuerpo']
+         }
+        result.append(jsonData)
+    client.close()
+    return JsonResponse(result, safe=False)
